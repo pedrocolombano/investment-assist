97aa6014ad6334ff7c8c91ad9c83aa47cc490d03
Pedro Colombano <pedromaiacolombano@gmail.com>
2026-05-30 12:15:44 -0300

    feat: create project

---
+++ src/main/java/br/com/pedrocolombano/investmentassist/InvestmentAssistApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrocolombano.investmentassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentAssistApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(InvestmentAssistApplication.class, args);
+    }
+
+}
